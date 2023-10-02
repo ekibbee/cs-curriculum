f37ed4e5189ab1a31cbcf0d26e4d9e7f4e81aa42
ekibbee <144268646+ekibbee@users.noreply.github.com>
2023-10-02 11:40:23 -0700

    started code for collecting coins

---
+++ Assets/Scripts/Scripts/PlayerMovementOW.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+float timer;
+bool iframes;
+int health;
+int gold;
+
+public class PlayerMovementOW : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private float walkingSpeed;
+    private float xDirection;
+    private float xVector;
+
+    public TextMeshProUGUI goldText;
+    public TextMeshProUGUI healthText;
+
+    void Start()
+    {
+        walkingSpeed = 5f;
+    }
+
+
+    void Update()
+    {
+        xDirection = Input.GetAxis("Horizontal");
+        xVector = xDirection * walkingSpeed * Time.deltaTime;
+        transform.position = transform.position + new Vector3(xVector, 0, 0);
+        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if()
+    }
+}
